8010235a5c15618e9c1790ac2006ed148caf450d
username@email.com <279023678@qq.com>
2020-07-06 00:36:28 +0800

    Config配置中心服务端以及客户端；

---
+++ cloud-config-client-3355/src/main/java/com/lzh/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.lzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ConfigClientMain3355
+ * @Description TODO
+ * @Author liuzhihui
+ * @Date 2020/7/5 23:14
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
+
