3b08388c8063e9f057673fe1581d4aaaca37106f
username@email.com <279023678@qq.com>
2020-07-07 01:00:14 +0800

    Bus动态刷新全局广播配置实现；

---
+++ cloud-config-client-3366/src/main/java/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ConfigClientMain3366
+ * @Description TODO
+ * @Author liuzhihui
+ * @Date 2020/7/5 23:14
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3366.class,args);
+    }
+}
+
---
+++ cloud-config-client-3366/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//将配置信息以rest风格暴露
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.application.name}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo+" "+port;
+    }
+}
