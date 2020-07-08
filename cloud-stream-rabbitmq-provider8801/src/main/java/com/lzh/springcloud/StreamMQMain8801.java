3db6824e7d85103323c677afacfcec37d2af29ba
username@email.com <279023678@qq.com>
2020-07-08 15:11:36 +0800

    rabbitmq提供者

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lzh/springcloud/StreamMQMain8801.java
@@ -0,0 +1,18 @@
+package com.lzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName StreamMQMain8801
+ * @Description TODO
+ * @Author liuzhihui
+ * @Date 2020/7/7 23:20
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class StreamMQMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8801.class, args);
+    }
+}
