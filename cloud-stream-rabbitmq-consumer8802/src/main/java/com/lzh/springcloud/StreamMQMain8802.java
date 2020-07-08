493a6dc3557c3f9dbd3c64db8bf8b096fa670c27
username@email.com <279023678@qq.com>
2020-07-09 00:13:08 +0800

    消息驱动消费者8802/8803

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/lzh/springcloud/StreamMQMain8802.java
@@ -0,0 +1,18 @@
+package com.lzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName StreamMQMain8802
+ * @Description TODO
+ * @Author liuzhihui
+ * @Date 2020/7/8 15:15
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,29 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName ReceiveMessageListenerController
+ * @Description TODO
+ * @Author liuzhihui
+ * @Date 2020/7/8 18:25
+ * @Version 1.0
+ **/
+@Component
+//定义消息的推送管道
+//Sink表示：从Stream接受消息就是输入，消费者
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+    //监听队列，用于消费者的队列的消息接受
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者1号，接受："+message.getPayload()+"\t port:"+serverPort);
+    }
+}
